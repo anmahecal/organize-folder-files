28ad2e702c68897efb2577a00f8f6f6b5cf755fa
anmahecal <andresmhenao@gmail.com>
2020-11-16 21:21:23 -0500

    :mechanic: Fixed FileExistsError when moving file to destination folder by addind a consecutive number

--- groot/Mover.py
+++ groot/Mover.py
@@ -27,6 +27,9 @@ class Mover():
     def move_file(self, filename:str):
         """Move file from source directory to destination directory"""
 
+        if os.path.isdir(os.path.join(self.src_path, filename)):
+            return None
+
         self._split_ext(filename)
 
         # Check current filename extension in supported extensions
@@ -39,10 +42,47 @@ class Mover():
                     os.mkdir(destination_path)
                     print(f'Creating directory {destination_path}')
                     print(f'Moving -> {filename} to {destination_path}')
-                os.rename(self._get_current_file_path(), self._set_next_file_path(folder_name))
+                try:
+                    os.rename(self._get_current_file_path(), self._set_next_file_path(folder_name))
+                except FileExistsError as e:
+                    new_filename = self._add_consecutive_to_filename(filename)
+
+                    # Change the filename inside current folder
+                    os.rename(self._get_current_file_path(), self._get_current_file_path(new_filename))
+
+                    # Try to move the file recursively
+                    self.move_file(new_filename)
+                
 
             # TODO: Files that don't match any of the supported extensions should stay
             # in source folder or create a folder for all of them?
+
+
+    def _add_consecutive_to_filename(self, filename: str) -> str:
+        """Concatenate a number at the end of the filename if filename already exists in destination"""
+
+        import re
+
+        file, ext = os.path.splitext(filename)
+        pattern = '\(\d+\)' # (1), (33)
+        # TODO: This pattern has to be at the end of the file name
+        is_pattern = re.findall(pattern, file)
+        if is_pattern:
+            new_filename = self._increment_consecutive_number(file, is_pattern[0]) + ext
+        else:
+            new_filename = file + '(1)' + ext
+        return new_filename
+
+
+    @staticmethod
+    def _increment_consecutive_number(file: str, pattern: str) -> str:
+        """Search for consecutive number in parenthesis, add one and return the new filename"""
+        
+        print(f'file: {file}, patt: {pattern}')
+        cons_number = int(pattern.lstrip('(').rstrip(')'))
+        new_cons_number = cons_number + 1
+        new_cons_str = f'({new_cons_number})'
+        return file.replace(pattern, new_cons_str)
 
 
     def _split_ext(self, filename:str):
@@ -54,16 +94,23 @@ class Mover():
         self.file_ext = file_ext
 
 
-    def _get_current_file_path(self) -> str:
+    def _get_current_file_path(self, filename=None) -> str:
         """Return the full current path to the file"""
 
-        return os.path.join(self.src_path, self.filename)
+        if not filename:
+            filename = self.filename
 
 
-    def _set_next_file_path(self, folder_name:str) -> str:
+        return os.path.join(self.src_path, filename)
+
+
+    def _set_next_file_path(self, folder_name:str, filename=None) -> str:
         """Return the destination path to the file"""
         
-        return os.path.join(self.src_path, folder_name, self.filename)
+        if not filename:
+            filename = self.filename
+
+        return os.path.join(self.src_path, folder_name, filename)
 
 
     def __repr__(self):
